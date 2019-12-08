1db72811c22b2fbad8745c9acb998eaa4e515ea4
zheng <2353820963@qq.com>
2019-12-08 16:08:22 +0800

    查询一个员工实现

--- src/main/java/com/baizhi/service/EmpServiceImpl.java
+++ src/main/java/com/baizhi/service/EmpServiceImpl.java
@@ -10,45 +10,61 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 @Service
 @Transactional
 public class EmpServiceImpl implements EmpService {
     @Autowired
     private EmpDao empDao;
+
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public Map findAll() {
-        Map map=new HashMap();
-        String message="";
-        String status="";
+        Map map = new HashMap();
+        String message = "";
+        String status = "";
         List<Emp> emps = null;
         try {
             emps = empDao.selectAll();
-            message="ok";
-            status="200";
-            map.put("emps",emps);
+            message = "ok";
+            status = "200";
+            map.put("emps", emps);
         } catch (Exception e) {
             e.printStackTrace();
-            message="no ok";
-            status="-200";
+            message = "no ok";
+            status = "-200";
         }
-        map.put("status",-200);
-        map.put("message","no ok");
+        map.put("status", -200);
+        map.put("message", "no ok");
         return map;
     }
+
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public Map findOne(String id) {
-        return null;
+        Map map = new HashMap();
+        try {
+            Emp emp = empDao.selectByPrimaryKey(id);
+            map.put("emp", emp);
+            map.put("status", 200);
+        } catch (Exception e) {
+            e.printStackTrace();
+            map.put("status", -200);
+            map.put("message", "查询失败");
+        }
+        return map;
     }
+
     @Override
     public Map delete(String id) {
         return null;
     }
+
     @Override
     public Map add(Emp emp) {
         return null;
     }
+
     @Override
     public Map update(Emp emp) {
         return null;
